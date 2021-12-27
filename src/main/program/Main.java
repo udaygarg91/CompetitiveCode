0912c0ca30da043fdd345339c2c4bf896e893090
Uday Garg <36131155+udaygarg91@users.noreply.github.com>
2021-12-27 11:44:38 +0530

    leetpractice 2

--- src/main/program/Main.java
+++ src/main/program/Main.java
@@ -1,28 +1,58 @@
-package main.program;
+package main.epam;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+
 public class Main {
-	public static void main(String[] args) {
-		Person[] arr = new Person[] { new Person(2, "ba", "GoldA"),
-				new Person(3, "ca", "SilverA"),
-				new Person(1, "aa", "PlatinumB"), 
-				new Person(3, "cb", "SilverB"),
-				new Person(3, "cc", "SilverC"),
-				new Person(2, "bb", "GoldB"),
-				new Person(1, "ab", "PlatinumA"),
-				new Person(1, "ac", "PlatinumC"),
-				new Person(2, "bc", "GoldC") };
 
-		for (int i = 0; i < arr.length; i++) {
-			for (int j = 0; j < arr.length - i - 1; j++) {
-				if (arr[j + 1].classId < arr[j].classId) { // 1(a) 1(b) 1(c) 1(d)
-					Person tmp = arr[j + 1];				//1b 1c 1d 1a 
-					arr[j + 1] = arr[j];
-					arr[j] = tmp;
-				}
+	public static void main(String[] args) throws Exception {
+
+		Thread t1 = new Thread(() -> {
+			try {
+				System.out.println(Singleton.getInstanse().hashCode());
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
 			}
-		}
-		for (Person person : arr) {
-			System.out.println(person);
-		}
-	}
+		});
 
+		Thread t2 = new Thread(() -> {
+			try {
+				System.out.println(Singleton.getInstanse().hashCode());
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		});
+
+//		t1.start();
+//		t2.start();
+
+		Singleton s = Singleton.getInstanse();
+		Singleton s1 = (Singleton) s.clone();
+
+		//System.out.println(s.hashCode());
+		System.out.println(s1.hashCode());
+
+//		ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream("abc.acr"));
+//		oos.writeObject(s);
+//		oos.flush();
+//		oos.close();
+//		ObjectInputStream obj = new ObjectInputStream(new FileInputStream("abc.acr"));
+//		Singleton test = (Singleton) obj.readObject();
+//		obj.close();
+
+		//System.out.println(test.hashCode());
+
+		Class cc = s1.getClass();
+		Constructor []arr = cc.getDeclaredConstructors();
+		arr[0].setAccessible(true);
+		Singleton p = (Singleton) arr[0].newInstance();
+		System.out.println(p.hashCode());
+	}
 }
